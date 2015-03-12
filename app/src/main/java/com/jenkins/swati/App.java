992299d49caed9957c74ffd39d19a3e41325043c
SwatiKK <swati_kinge@persistent.co.in>
2015-03-12 14:29:52 +0530

    Initial commit

---
+++ app/src/main/java/com/jenkins/swati/App.java
@@ -0,0 +1,13 @@
+package com.jenkins.swati;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
